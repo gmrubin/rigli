b00b618e930c03f2eaf2b4ffa2fd194614dcb4d7
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-06-13 19:54:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rigli::Application.config.secret_token = '43528f563449123bc62e81ec600dfce6b9e81df3ee738559b2349b4e0150958b4aa855927b28960af91bb63d0be134a51c9e5a96e71807413c732d66c184e5ee'
