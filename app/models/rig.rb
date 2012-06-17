7dea1c6d5d195c9ca36457e4aa75173a74d30c8a
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-06-16 23:04:43 -0400

    Adds valuations to rig and associates with link
    adds first cut of rig specs

--- app/models/rig.rb
+++ app/models/rig.rb
@@ -1,3 +1,5 @@
 class Rig < ActiveRecord::Base
   attr_accessible :description, :name
+  validates :name, :presence => true
+  has_many :links
 end
--- spec/models/rig_spec.rb
+++ spec/models/rig_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Rig do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:links) }
+
+  before(:each) do
+  @attr = {
+    name: "test rig",
+    description: "This rig is for testing"
+  }
+  end
+
+  it "should create a new Rig given valid attributes" do
+    Rig.create!(@attr)
+  end
+
+  it "should require an name" do
+    no_name_rig = Rig.new(@attr.merge(:name => ""))
+    no_name_rig.should_not be_valid
+  end
+
+
 end
