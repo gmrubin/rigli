357cbe740d0aee5bf9a968e094bae52318f0ff28
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-06-16 20:52:32 -0400

    Adds mailers config for dev and test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,7 @@ Rigli::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'example.com' }
 end
