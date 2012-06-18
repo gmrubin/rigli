5f81a9f67f924dc6db465e0add55e0909af9e95a
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-06-18 01:20:16 -0400

    First stab at shortened url generator

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,4 +1,15 @@
-class ShortenedUrl < ActiveRecord::Base
-  attr_accessible :shortened_object_id, :shortened_object_type, :surl
+class Shortened_url < ActiveRecord::Base
+  attr_accessible :shortened_object_id, :type, :surl
   belongs_to :shortened_object, polymorphic: true
+  before_create :generate_surl
+
+  private
+   def generate_surl
+    self.surl = rand(36**6).to_s(36)
+    if self.class.find_by_surl(surl)
+      generate_surl
+    else
+      surl
+    end
+   end
 end
